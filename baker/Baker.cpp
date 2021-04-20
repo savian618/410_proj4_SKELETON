9af69616fece1bf2f093751dc597b96576740e4c
savian618 <savian.elam.18@cnu.edu>
2021-04-20 17:00:00 -0400

    Baker done?

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -1,6 +1,7 @@
 #include <mutex>
 
 #include "../includes/baker.h"
+#include "../includes/externs.h"
 using namespace std;
 
 Baker::Baker(int id):id(id)
@@ -12,9 +13,37 @@ Baker::~Baker()
 }
 
 void Baker::bake_and_box(ORDER &anOrder) {
+	Box b;
+	int numD = anOrder.number_donuts;
 	
+	for(int i = 0; i < numD; i++){
+		DONUT d;
+		b.addDonut(d);
+		if(b.size() == DOZEN){
+			anOrder.boxes.push_back(b);
+			b.clear;
+		}
+	}
+	if(b.size > 0){
+		anOrder.boxes.push_back(b);
+		b.clear;
+	}
 }
 
 void Baker::beBaker() {
-	
+	while(true){
+		unique_lock<mutex> lck(mutex_order_inQ);
+		ORDER o;
+
+		while(order_in_Q.size() > 0 && b_WaiterIsFinished == true){
+			cv_order_inQ.wait(lck);
+
+		}
+		o = order_in_Q.front();
+		order_in_Q.pop();
+		bake_and_box(o);
+		lock_guard<mutex> lk(mutex_order_outQ);
+		order_out_Vector.push_back(o);
+
+	}
 }
